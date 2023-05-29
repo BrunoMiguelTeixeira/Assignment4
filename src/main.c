41c6a4fb949ea35d29c60b3669a8b57f58ef3b88
IALeite <ifaleite@ua.pt>
2023-05-30 00:38:46 +0100

    buttons and leds configuration, temperature semaphores

--- src/main.c
+++ src/main.c
@@ -17,6 +17,9 @@
 #define TASK_BUTTONS_PRIORITY 5
 #define TASK_LEDS_PRIORITY 5
 
+/*Semaphores definition*/
+K_SEM_DEFINE(temp_sem, 1, 1);
+
 /* Create threads stack space*/
 K_THREAD_STACK_DEFINE(task_temp_stack, STACK_SIZE);
 K_THREAD_STACK_DEFINE(task_buttons_stack, STACK_SIZE);
@@ -39,30 +42,76 @@ void LedsUpdate(void *argA, void *argB, void *argC);
 
 /*----*/
 static const struct i2c_dt_spec dev_i2c = I2C_DT_SPEC_GET(I2C0_NODE);
+static const struct device *gpio0_dev = DEVICE_DT_GET(GPIO0_NODE);
 
 const uint8_t buttons_pins[] = {11, 12, 24, 25};
+const uint8_t leds_pins[] = {13, 14, 15, 16};
 
 uint8_t data = 0x00;
+
 int ret;
 int output_period = 10; /* ms */
-
+int input_period = 10;
 
 /*Real Time application database*/
 struct RTDB
 {
-	int LED0, LED1, LED2, LED3;
-	int BUT0, BUT1, BUT2, BUT3;
+	int LED0;
+	int LED1;
+	int LED2;
+	int LED3;
+	int BUT0;
+	int BUT1;
+	int BUT2;
+	int BUT3;
 	int TEMP;
 };
 
 void main(void)
 {
-	/* Check if the device is ready to be used,place holder for now */
+	/* Check if the i2c connected device is ready to be used */
 	if (!device_is_ready(dev_i2c.bus))
 	{
 		printk("I2C: Device is not ready.\n");
 		return;
 	}
+	else
+	{
+		printk("I2C: Device is ready!\n");
+	}
+
+	/*Configure Buttons*/
+	int i, ret;
+	for (i = 0; i < sizeof(buttons_pins); i++)
+	{
+		ret = gpio_pin_configure(gpio0_dev, buttons_pins[i], GPIO_INPUT | GPIO_PULL_UP);
+		if (ret < 0)
+		{
+			printk("Error: gpio_pin_configure failed for button %d/pin %d, error:%d\n\r", i + 1, buttons_pins[i], ret);
+			return;
+		}
+		else
+		{
+			printk("Success: gpio_pin_configure for button %d/pin %d\n\r", i + 1, buttons_pins[i]);
+		}
+	}
+
+	/*Configure Leds*/
+	for (i = 0; i < sizeof(leds_pins); i++)
+	{
+		ret = gpio_pin_configure(gpio0_dev, leds_pins[i], GPIO_OUTPUT_ACTIVE);
+		if (ret < 0)
+		{
+			printk("Error: gpio_pin_configure failed for led %d/pin %d, error:%d\n\r", i + 1, leds_pins[i], ret);
+			return;
+		}
+		else
+		{
+			printk("Success: gpio_pin_configure for led %d/pin %d\n\r", i + 1, leds_pins[i]);
+		}
+	}
+
+
 
 	/* Thread creation */
 	task_temp_tid = k_thread_create(&task_temp_data, task_temp_stack,
@@ -94,7 +143,14 @@ void TemperatureUpdate(void *argA, void *argB, void *argC)
 		ret = i2c_read_dt(&dev_i2c, &data, sizeof(data));
 
 		printk("Read %dºCelcius\n", data);
-		/* Make thread sleep for the remaining of time before next check */
+
+		k_sem_take(&temp_sem, K_FOREVER); 
+
+		// RTDB.TEMP;
+
+		k_sem_give(&temp_sem);
+
+			/* Make thread sleep for the remaining of time before next check */
 		if (k_uptime_get() - start_time < output_period)
 		{
 			/*gets the most updated remaining time to sleep*/
